32f71c6bd5ec381042682f2422a2cb1c95794de8
subhashis sarkar <ssarkarjpg03@gmail.com>
2024-03-24 18:30:19 +0530

    new

--- javascript/app.js
+++ javascript/app.js
@@ -6,6 +6,15 @@ const choices = document.querySelectorAll(".choise");
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
+const hands = document.querySelector(".hands");
+// const changeimg = document.querySelector(".hands2");
+const userrock  = document.querySelector("#user-rock");
+const userpaper = document.querySelector("#user-paper");
+const userscissor = document.querySelector("#user-scissor");
+const pcrock = document.querySelector("#pc-rock");
+const pcpaper = document.querySelector("#pc-paper");
+const pcscissor = document.querySelector("#pc-scissor");
+
 
 function party() {
     confetti();
@@ -29,27 +38,30 @@ const showWinner = (userWin) => {
         userScore ++;
         userScorePara.innerText = userScore;
         // console.log("You win");
-        msg.innerText = "You Win";
-        msg.style.backgroundColor = "green";
-        if(userScore === 10) {
-            let repeate = setInterval(party, 1000);
-            choise.removeEventListener("click",clickHandler);
-            setTimeout(function() {
+        setTimeout(function() {                
+            msg.innerText = "You Win";
+            msg.style.backgroundColor = "green";
+        },1000)
+        if(userScore >= 3) {
+            let repeate = setInterval(party, 1000);                       
+            setTimeout(function() {                
                 window.location.reload();
-            },5000)
+            },3500)
         }
     }
     else {
         compScore ++;
         compScorePara.innerText = compScore;
         // console.log("You lose");
-        msg.innerText = "You lose";
-        msg.style.backgroundColor = "red";
-        if(compScore === 10) {
-            // let repeate = setInterval(party, 1000);
-            setTimeout(function() {
+        setTimeout(function() {               
+            msg.innerText = "You lose";
+            msg.style.backgroundColor = "red";
+        },1000)
+        if(compScore >= 3) {
+            // let repeate = setInterval(party, 1000);          
+            setTimeout(function() {               
                 window.location.reload();
-            },5000)
+            },3500)
         }
     }
 }
@@ -62,6 +74,9 @@ const playgame = (userchoise) => {
 
     if(userchoise === compChoise) {
         drawGame();
+        // hands.style.display = "none";
+        // userrock.style.display = "flex";
+        // pcrock.style.display = "flex";
     }
     else {
         let userWin = true;
@@ -80,6 +95,96 @@ const playgame = (userchoise) => {
         showWinner(userWin);
     }
 
+    if(userchoise === "rock" && compChoise === "rock") {
+        hands.style.display = "none";
+        userrock.style.display = "flex";
+        pcrock.style.display = "flex";        
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userrock.style.display = "none";
+            pcrock.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "paper" && compChoise === "paper") {
+        hands.style.display = "none";
+        userpaper.style.display = "flex";
+        pcpaper.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userpaper.style.display = "none";
+            pcpaper.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "scissors" && compChoise === "scissors") {
+        hands.style.display = "none";
+        userscissor.style.display = "flex";
+        pcscissor.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userscissor.style.display = "none";
+            pcscissor.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "rock" && compChoise === "paper") {
+        hands.style.display = "none";
+        userrock.style.display = "flex";
+        pcpaper.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userrock.style.display = "none";
+            pcpaper.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "rock" && compChoise === "scissors") {
+        hands.style.display = "none";
+        userrock.style.display = "flex";
+        pcscissor.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userrock.style.display = "none";
+            pcscissor.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "paper" && compChoise === "rock") {
+        hands.style.display = "none";
+        userpaper.style.display = "flex";
+        pcrock.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userpaper.style.display = "none";
+            pcrock.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "paper" && compChoise === "scissors") {
+        hands.style.display = "none";
+        userpaper.style.display = "flex";
+        pcscissor.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userpaper.style.display = "none";
+            pcscissor.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "scissors" && compChoise === "rock") {
+        hands.style.display = "none";
+        userscissor.style.display = "flex";
+        pcrock.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userscissor.style.display = "none";
+            pcrock.style.display = "none";
+        },3000)
+    }
+    else if(userchoise === "scissors" && compChoise === "paper") {
+        hands.style.display = "none";
+        userscissor.style.display = "flex";
+        pcpaper.style.display = "flex";
+        setTimeout(function() {
+            hands.style.display = "flex";
+            userscissor.style.display = "none";
+            pcpaper.style.display = "none";
+        },3000)
+    }
 };
 
 choices.forEach((choise) => {
@@ -90,6 +195,3 @@ choices.forEach((choise) => {
         playgame(userchoise);
     })
 });
-
-
-
